0db992579bcfc698936fed12fb05b1bb0a4297d7
Mohammed Khalil <muhamad.khalil12345@gmail.com>
2022-07-24 01:46:44 +0300

    push naw

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,23 +1,25 @@
+import { data } from 'autoprefixer';
 import React, { Fragment, useEffect, useState, createContext} from 'react';
 export const userCon=createContext()
 
 
 export default function Home() {
-  let datanaw=[
-    { name:'ahmad'},
-    { name:'Mhamad'},
-    { name:'Azad'},
-   ]
+
+
   const [Item, setItem] = useState({
-    name:''
+    name:'',id:3
   });
+  let [datanaw, setNaw] = useState([
+    { name:'ahmad',id:1},
+    { name:'Mhamad',id:2},
+  ]);
   const Increase= ()=>{
     setItem((data)=>{
       return {...data,name:'hamataza'}
     })
   }
   const Add =()=>{
-    datanaw.push(Item)
+    setNaw([...datanaw,Item])
     setItem({name:''})
     console.log(datanaw);
   }
@@ -33,21 +35,19 @@ export default function Home() {
           <div className="header mt-2 flex justify-around">
             <input type="text" className='h-10 bg-slate-50 text-gray-900
                              placeholder:text-gray-900 p-2 focus:outline-none rounded-md' placeholder='Write new item'
-            onChange={(event)=>{setItem({name:event.target.value})}} value={Item.name} />
+            onChange={(event)=>{setItem({name:event.target.value ,id:3})}} value={Item.name} />
             <button onClick={Add} className='bg-green-500 hover:bg-green-600 w-16 p-2 rounded-md'>Add</button>
           </div>
-
           <hr className='opacity-30 bg-slate-700 w-full h-[2px] mt-1'/>
-          
           {datanaw.map((event)=>{
-            return <div className='text-black w-[98%] m-auto mt-1 rounded-md
+            return (<div className='text-black w-[98%] m-auto mt-1 rounded-md
                        hover:bg-slate-400 p-2 bg-slate-100 flex justify-between' key={event.name}>
               <div className="name">{event.name}</div>
               <div className="name flex">
                 <button className='text-red-700'>Del</button>
                 <button className='text-blue-700'>Up</button>
               </div>
-            </div>
+            </div>)
           })}
         </div>
       </div>
